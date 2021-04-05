b48b2b05776b712018116076908724505e331a97
Jaewoo Park <jaewoopark@Mac-mini.local>
2021-04-05 21:28:29 +0900

    set_spring

---
+++ my-first-spring-boot-app-7/src/main/java/org/cnu/realcoding/myfirstspringbootapp7/MyFirstSpringBootApp7Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.myfirstspringbootapp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootApp7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootApp7Application.class, args);
+	}
+
+}
---
+++ my-first-spring-boot-app-7/src/test/java/org/cnu/realcoding/myfirstspringbootapp7/MyFirstSpringBootApp7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.myfirstspringbootapp7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootApp7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
