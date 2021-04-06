ff4f6cb79f16fbb2f514ad868f20cc2f06b70925
jwoo9928 <51700184+jwoo9928@users.noreply.github.com>
2021-04-06 13:51:30 +0900

    Update Dogrepository.java

--- my-first-spring-boot-app-7/src/main/java/org/cnu/realcoding/repository/Dogrepository.java
+++ my-first-spring-boot-app-7/src/main/java/org/cnu/realcoding/repository/Dogrepository.java
@@ -3,6 +3,7 @@ package org.cnu.realcoding.repository;
 
 import org.cnu.realcoding.domain.Dog;
 import org.cnu.realcoding.exception.DogsNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -10,11 +11,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class Dogrepository {
+    @Autowired
     private MongoTemplate mongoTemplate;
 
     public boolean checkDog(String name, String o_name, String number) {
         Dog dog = mongoTemplate.findOne(Query.query(Criteria.where("name").is(name)),Dog.class);
-        if(dog!= null ||(dog.getOwnerPhoneNumber().equals(number) && dog.getOwnerName().equals(o_name))) {
+        if(dog == null ||!(dog.getOwnerPhoneNumber().equals(number) && dog.getOwnerName().equals(o_name))) {
             return true;
         }
         throw new DogsNotFoundException();
