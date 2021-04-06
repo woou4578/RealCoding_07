51ed3797937e444669aa65697f3d2fd1fc62f787
jwoo9928 <51700184+jwoo9928@users.noreply.github.com>
2021-04-06 14:31:49 +0900

    Add files via upload

---
+++ my-first-spring-boot-app-7/src/main/java/org/cnu/realcoding/exception/DogsNotFoundException.java
@@ -0,0 +1,8 @@
+package org.cnu.realcoding.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DogsNotFoundException extends RuntimeException{
+}
